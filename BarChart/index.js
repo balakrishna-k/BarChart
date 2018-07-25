d38e0d9bbaa77adc9ae554cae73b324b973ff679
balakrishna-k <kabalakr@ucsc.edu>
2018-07-25 00:44:29 -0700

    Added transitions and other fixes

--- BarChart/index.js
+++ BarChart/index.js
@@ -9,68 +9,49 @@ var h = +svg.attr("height") - margin.top - margin.bottom;
 
 var formatDecimal = d3.format(".1f");
 
-//var xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1); // See how it looks with 0 padding as well
-//var yScale = d3.scaleLinear().range([height, 0]);
-
 drawStackedBarChart("data.csv", 0);
 
-function drawStackedBarChart(dataFileName, selectNumber) {
+function drawStackedBarChart(dataFileName) {
     
     d3.csv(dataFileName).then(function (data) {
-        
-        //var categoryColors = d3.scaleOrdinal(d3.schemeCategory10);
         
         var columns = data.columns;
         var categories = columns.slice(1);
         
-        var stack = d3.stack().keys(categories).order(d3.stackOrderDescending)
+        var stack = d3.stack().keys(categories);
         var colors = d3.scaleOrdinal().domain(categories)
-            .range(["#fff7fb", "#ece2f0", "#d0d1e6", "#a6bddb", "#67a9cf", "#3690c0", "#02818a", "#016c59", "#014636"]);
+            .range(["#d0d1e6", "#a6bddb", "#67a9cf", "#3690c0", "#02818a", "#016c59", "#014636"]);
         
-        /* valid numbers from 0 to length of categories - 1 */
-        if (selectNumber > categories.length-1 || selectNumber < 0) {
-            console.warn("Invalid Category Selected | Max number is " + (categories.length -1) + " | Defaulting to 0");
-            selectNumber = 0;
-        }
-        
-        var select = selectNumber; 
-        var selectedCategory = categories[select];
-
-        //data.sort(function (a,b) {return b[selectedCategory] - a[selectedCategory]; }); // Sorting the data from highest to lowest
-
-        //var states = data.map(function (d) { return d[columns[0]]; }); // Simply returns and array with all the states
-
         var dataset =[];
         data.forEach(function (datum) {
             var tempObj = {};
-          
+            var sum = 0;
             categories.forEach(function (category) {
-                tempObj[category] = +datum[category];    
+                tempObj[category] = +datum[category];
+                sum = sum + tempObj[category];
+                
             });
+            tempObj.total = sum;
             dataset.push(tempObj); 
         });
         
-        
-        var stackedData = stack(dataset);
-        console.log(stackedData);
+        console.log(dataset);
         
         var xScale = d3.scaleBand()
 				.domain(d3.range(dataset.length))
 				.range([0, w])
-				.paddingInner(0.1);
+				.paddingInner(0.1).align(0.1);
         
         var yScale = d3.scaleLinear()
-				.domain([0,	d3.max(dataset, function(d) {    
-                    var sum = 0;    
-                    categories.forEach(function (category) {
-                        sum = sum + d[category];        
-                    });	
-                        
-                    return sum;
+				.domain([0,	d3.max(dataset, function(d) {                       
+                    return d.total;
                 })])
 				.range([h, 0]);
         
+        dataset.sort(function (a,b) {return b.total - a.total; }); // Sorting the data from highest to lowest
         
+        var stackedData = stack(dataset);
+    
         var groups = svg.selectAll("g")
 				.data(stackedData)
 				.enter()
@@ -87,12 +68,17 @@ function drawStackedBarChart(dataFileName, selectNumber) {
 					return xScale(i);
 				})
 				.attr("y", function(d) {
-					return yScale(d[1]);
+					return h;
 				})
+                .attr("height", 0)
+                .transition().duration(1800).delay(function (d,i) {return i*20;})
 				.attr("height", function(d) {
 					return yScale(d[0]) - yScale(d[1]);
 				})
-				.attr("width", xScale.bandwidth());
+				.attr("y", function(d) {
+					return yScale(d[1]);
+				})
+                .attr("width", xScale.bandwidth());
         
     });
 }
